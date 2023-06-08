b94f7ea5b878f453b153a2cc6b041bd4c9ae5c47
xenonth <timothy.forbes259@gmail.com>
2023-06-08 15:57:33 +0930

    rendering dice

--- js/main.js
+++ js/main.js
@@ -7,4 +7,36 @@ const params = {
     edgeRadius: .07
 };
 
+//variable for boxGeometry
 let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
+
+//Dice maker function 
+
+function createDiceGeometry() {
+    let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
+    
+    const positionAttribute = boxGeometry.attributes.position;
+    const subCubeHalfSize = .5 - params.edgeRadius;
+
+    for (let i = 0; i < positionAttribute.count; i++) {
+
+        let position = new THREE.Vector3().fromBufferAttribute(positionAttribute, i);
+
+        // modify position.x, position.y and position.z
+
+        positionAttribute.setXYZ(i, position.x, position.y, position.z);
+
+        if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            // position is close to box vertex
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize) {
+            // position is close to box edge that's parallel to Z axis
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            // position is close to box edge that's parallel to Y axis
+        } else if (Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            // position is close to box edge that's parallel to X axis
+        }
+        
+    }
+
+    return boxGeometry;
+}
