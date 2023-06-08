4b99f0de6672ab6fa221540a15a3cbd95336b295
xenonth <timothy.forbes259@gmail.com>
2023-06-08 16:08:30 +0930

    code for rounded cube

--- js/main.js
+++ js/main.js
@@ -20,29 +20,30 @@ function createDiceGeometry() {
 
     for (let i = 0; i < positionAttribute.count; i++) {
 
+           
         let position = new THREE.Vector3().fromBufferAttribute(positionAttribute, i);
 
-                //setting rounded edge
-                const subCube = new THREE.Vector3(
-                    Math.sign(position.x),
-                    Math.sign(position.y),
-                    Math.sign(position.z)
-                ).multiplyScalar(subCubeHalfSize);
-            
-                const addition = new THREE.Vector3().subVectors(position, subCubeEdges);
-
-        // modify position.x, position.y and position.z
-
-        positionAttribute.setXYZ(i, position.x, position.y, position.z);
+        const subCube = new THREE.Vector3(Math.sign(position.x), Math.sign(position.y), Math.sign(position.z)).multiplyScalar(subCubeHalfSize);
+        const addition = new THREE.Vector3().subVectors(position, subCube);
 
         if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            // position is close to box vertex
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position = subCube.add(addition);
         } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize) {
-            // position is close to box edge that's parallel to Z axis
+            addition.z = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.y = subCube.y + addition.y;
         } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            // position is close to box edge that's parallel to Y axis
+            addition.y = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.z = subCube.z + addition.z;
         } else if (Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
-            // position is close to box edge that's parallel to X axis
+            addition.x = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.y = subCube.y + addition.y;
+            position.z = subCube.z + addition.z;
         }
 
         
