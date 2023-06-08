6e0c6fa3e41baaf88deec03c3bf48e2602761ac2
xenonth <timothy.forbes259@gmail.com>
2023-06-08 15:36:41 +0930

    setup imports for main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const test = "test time";
+import * as CANNON from 'https://cdn.skypack.dev/cannon-es';
+import * as THREE from 'three';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+
+const params = {
+    segments: 50,
+    edgeRadius: .07
+};
+
+let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
